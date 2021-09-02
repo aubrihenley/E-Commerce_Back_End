2bfa68f82a478d6b757c3716c4d58eaf0d09eb8c
Aubri Henley <aubri.henley@gmail.com>
2021-09-02 14:05:40 -0400

    updated Models index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey:'',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey:'',
+  onDelete:'Cascade',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag,
+foreignKey:'',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag,
+foreignKey:'',
+});
+
 
 module.exports = {
   Product,
