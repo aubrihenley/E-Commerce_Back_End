e999b79d6ecbba9caf3fcd6d39d78546494afa1f
Aubri Henley <aubri.henley@gmail.com>
2021-09-03 12:26:59 -0400

    added category route by id value and create category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,13 +15,32 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include:[Product],
+    });
+    if(!categoryData){
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
