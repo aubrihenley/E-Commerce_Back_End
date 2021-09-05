148fbe3150f8b491ecc1e6f298ee0d53ccc857dc
Aubri Henley <aubri.henley@gmail.com>
2021-09-05 11:26:44 -0400

    added foreign keys to associations in index.js models folder

--- models/index.js
+++ models/index.js
@@ -18,11 +18,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag,
   unique: false,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag,
   unique: false,
+  foreignKey: 'tag_id',
 });
 
 
